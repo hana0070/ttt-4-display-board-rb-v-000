f45fe29f21a45332cf65fa2f471f7706f0da861e
HANA LEANDER <hana0070@gmail.com>
2018-11-26 17:23:53 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,5 +3,9 @@
 
 board = [" "," "," "," "," "," "," "," "," "]
 def display_board(board)
-  
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end
